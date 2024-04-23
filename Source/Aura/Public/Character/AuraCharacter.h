1e7b07c8e710ca1866412c9d94f4a2b234debc84
jumpthe11 <jumpthe11@gmail.com>
2024-04-23 20:33:22 +0300

    23.04.24

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuroaCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCharacter : public AAuroaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
